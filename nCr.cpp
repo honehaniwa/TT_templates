248052faec0fd2fe1c21bdfcd5ab5c96df16282d
TT <trttkm@icloud.com>
2020-03-29 02:25:08 +0900

    組み合わせ(Combination nCr)

--- nCr.cpp
+++ nCr.cpp
@@ -1,24 +1,26 @@
-#define MAX_N 200005
-ll factorial[MAX_N];
-void make_factorial_table() {
+int mod = 1e9 + 7;
+
+#define MAX_N 20000500
+long long factorial[20000500];
+void init_table() {
 	factorial[0] = 1;
 	for (int i = 1; i < MAX_N; i++) {
 		factorial[i] = factorial[i - 1] * i%mod;
 	}
 }
 
-ll bisection_power(ll x, ll y) {
+long long bisection_power(long long x, long long y) {
 	if (y == 0) { return 1; }
-	ll t = bisection_power(x*x%mod, y / 2);
+	long long t = bisection_power(x*x%mod, y / 2);
 	if (y % 2 == 1) { t = t * x%mod; }
 	return t;
 }
 
-ll nCr(int n, int r) {
+long long nCr(int n, int r) {
 	if (n<r || n <= 0 || r<0) { return 0; }
-	ll a = factorial[n];
-	ll b = a * bisection_power(factorial[r], (mod - 2)) % mod;
-	ll c = b * bisection_power(factorial[n - r], mod - 2) % mod;
+	long long a = factorial[n];
+	long long b = a * bisection_power(factorial[r], (mod - 2)) % mod;
+	long long c = b * bisection_power(factorial[n - r], mod - 2) % mod;
 	return c % mod;
 }
 
@@ -27,7 +29,7 @@ ll nCr(int n, int r) {
 -------------How to use(copy it!)-------------
 
 //init
-make_factorial_table();
+init_table();
 
 //where you want to use nCr (a and b must be a integer(or long long))
 ans = nCr(a,b);
