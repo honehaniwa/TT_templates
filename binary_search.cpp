db1764b49569b09e1b8374c7599bdebd3cc1c09a
TT <trttkm@icloud.com>
2020-03-29 02:19:54 +0900

    二分探索

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,20 +1,31 @@
-bool isOK(int mid, double key) {
-	double y = (double)mid / (double)n;
-	//cout << sqrt(key + y * y) << " ";
-	if (sqrt(key + (y * y)) <= 1) return true;
+bool isOK(int mid) {
+	if (mid>100) return true;//ここは問題に合わせて条件式を変える
 	else return false;
 }
 
-int binary_search(double key) {
+int binary_search(long long n) {
 	int ng = -1; //「index = 0」が条件を満たすこともあるので、初期値は -1
 	int ok = n; // 「index = a.size()-1」が条件を満たさないこともあるので、初期値は a.size()
 				// ok と ng のどちらが大きいかわからないことを考慮
 	while (abs(ok - ng) > 1) {
 		int mid = (ok + ng) / 2;
 		//cout << mid << " " << key << endl;
-		if (!isOK(mid, key)) ok = mid;
+		if (isOK(mid)) ok = mid;
 		else ng = mid;
 	}
 
 	return ok;
 }
+
+/*
+main内に書きたいとき用
+
+long long ng = 0,ok = 1<<18;
+
+while (abs(ok - ng) > 1) {
+	long long mid = (ok + ng) / 2;
+	if (条件式) ok = mid;
+	else ng = mid;
+}
+
+*/
