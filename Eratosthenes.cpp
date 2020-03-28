c256d646fa2d95fea130736bc08075ff7844f44b
TT <trttkm@icloud.com>
2020-03-29 02:22:54 +0900

    エラトステネスの篩

--- Eratosthenes.cpp
+++ Eratosthenes.cpp
@@ -1,14 +1,14 @@
-vector<bool> IsPrime;
-
-void sieve(ll max) {
+vector<bool> sieve(long long max) {
+	vector<bool> IsPrime;
 	if (max + 1 > IsPrime.size()) {     // resize‚Å—v‘f”‚ªŒ¸‚ç‚È‚¢‚æ‚¤‚É
 		IsPrime.resize(max + 1, true); // IsPrime‚É•K—v‚È—v‘f”‚ğŠm•Û
 	}
 	IsPrime[0] = false; // 0‚Í‘f”‚Å‚Í‚È‚¢
 	IsPrime[1] = false; // 1‚Í‘f”‚Å‚Í‚È‚¢
 
-	for (ll i = 2; i*i <= max; ++i) // 0‚©‚çsqrt(max)‚Ü‚Å’²‚×‚é
+	for (long long i = 2; i*i <= max; ++i) // 0‚©‚çsqrt(max)‚Ü‚Å’²‚×‚é
 		if (IsPrime[i]) // i‚ª‘f”‚È‚ç‚Î
 			for (ll j = 2; i*j <= max; ++j) // (maxˆÈ‰º‚Ì)i‚Ì”{”‚Í
 				IsPrime[i*j] = false;      // ‘f”‚Å‚Í‚È‚¢
+	return IsPrime;
 }
